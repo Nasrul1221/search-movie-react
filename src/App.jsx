97fbfde37198f59ef5e84382d04bd719e7c2a0e0
nasrul1221 <fst20071221@gmail.com>
2025-04-25 15:17:33 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import React, {useState} from 'react';
+import './App.css';
+
+function App() {
+    const API_KEY = '74d15b84'
+    const [film, setFilm] = useState('')
+    const [search, setSearch] = useState('')
+
+    async function fetchFilm() {
+        const response_data = await fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&t=${search}`)
+        const data = await response_data.json()
+
+        setFilm(data)
+        console.log(data)
+    }
+
+    return (
+        <div className="app-container">
+            <div className={'input-container'}>
+                <input type={'text'} onChange={(e) => setSearch(e.target.value)} value={search} />
+                <button onClick={fetchFilm}>Search</button>
+            </div>
+
+            {film && (
+                <div className={'film-container'}>
+                    <div className={'film-left'}>
+                        <h1>{film.Title}</h1>
+                        <img src={film.Poster} alt={film.Title} />
+                        <div className={'film-left-info-container'}>
+                            <p className={'film-info'}>IMBd rating: {film.imdbRating}</p>
+                            <p className={'film-info'}>Votes: {film.imdbVotes}</p>
+                        </div>
+                    </div>
+                    <div className={'film-right'}>
+                        <p className={'film-info'}><span>Year:</span> {film.Year}</p>
+                        <p className={'film-info'}><span>Released:</span> {film.Released}</p>
+                        <p className={'film-info'}><span>Country:</span> {film.Country}</p>
+                        <p className={'film-info'}><span>Genre:</span> {film.Genre}</p>
+                        <p className={'film-info'}><span>Writer:</span> {film.Writer}</p>
+                        <div className={'plot-container'}>
+                            <p className={'plot'}>{film.Plot}</p>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default App
